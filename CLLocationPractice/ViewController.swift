03af38e121458ed6226a5373261d58b2691b33ac
Jae-eun <je4297@naver.com>
2019-05-16 02:01:15 +0900

    fetch location data

--- CLLocationPractice/ViewController.swift
+++ CLLocationPractice/ViewController.swift
@@ -7,14 +7,36 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    var locationManager = CLLocationManager()
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.startUpdatingLocation()
+        }
     }
 
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        if let userLocation = locations.first {
+            print("위도 \(userLocation.coordinate.latitude)")
+            print("경도 \(userLocation.coordinate.longitude)")
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        
+        print("location error \(error)")
+    }
 
 }
 
