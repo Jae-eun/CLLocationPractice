f21ae0b057c623b9395df8e039c46a4f94b7f9cf
Jae-eun <je4297@naver.com>
2019-05-16 01:45:18 +0900

    first commit

---
+++ CLLocationPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CLLocationPractice
+//
+//  Created by 이재은 on 16/05/2019.
+//  Copyright © 2019 Jaeeun Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
